67fde8ea8ef75b794fa2d7e8336624a8a952a886
jiseop121 <104052493+jiseop121@users.noreply.github.com>
2024-07-04 00:47:15 +0900

    Feat : Springboot 초기 세팅

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # Basketball_village
-농구 커뮤니티
+
+---
+# 목차
+# 서비스 설명
+# 구현 기능
+# 기술 스택
+| SpringBoot 의존성  |
+|:---------------:|
+|   Spring Boot   |
+|   Spring Web    |
+| Spring Data Jpa |
+| Bean Validation |
+|     Lombok      |
+|  MySQL Driver   |
+|    Security     |
+
+# 패키지 구조
+
+# API
+# ERD
---
+++ src/main/java/community/basketballvillage/BasketballvillageApplication.java
@@ -0,0 +1,13 @@
+package community.basketballvillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballvillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballvillageApplication.class, args);
+	}
+
+}
