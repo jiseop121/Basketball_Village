7c5771d3704db8a0573b219e4cf32ba934b85770
jiseop <ghdwltjq5749@naver.com>
2024-07-18 22:06:32 +0900

    Feat : dummyData prod에 적용 제외 및 ddl-auto create로 변경

--- src/main/java/community/basketballvillage/global/dummy/DummyData.java
+++ src/main/java/community/basketballvillage/global/dummy/DummyData.java
@@ -3,6 +3,7 @@ package community.basketballvillage.global.dummy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
@@ -14,6 +15,7 @@ import community.basketballvillage.repository.UserRepository;
 @Slf4j
 @Component
 @RequiredArgsConstructor
+@Profile({"dev","test"})
 public class DummyData {
 
     private final UserRepository userRepository;
